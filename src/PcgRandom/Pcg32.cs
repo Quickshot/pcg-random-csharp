2c621d8580b4d48e8f6c9d63f3a88d534c653ef7
Jukka Teittinen <jpteitti@gmail.com>
2016-01-22 21:12:15 +0200

    Partial implementation of random32. Currently missing NextByte, NextDouble, and Sample. Also comments/documentation are missing.

---
+++ src/PcgRandom/Pcg32.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PcgRandom
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class Pcg32 : Random
+    {
+        private ulong _state;
+        private ulong _inc;
+
+        public Pcg32() : this(unchecked((ulong)DateTime.Now.Ticks))
+        {
+        }
+
+        public Pcg32(int state) : this(unchecked((ulong)state))
+        {
+        }
+
+        public Pcg32(ulong state, ulong sequence = 0xda3e39cb94b95bdb)
+        {
+            if (sequence%2 == 1)
+            {
+                _state = state;
+                _inc = sequence;
+            }
+            else
+            {
+                throw new ArgumentException($"{sequence} is not an odd number.", nameof(sequence));
+            }
+        }
+
+        public uint Random()
+        {
+            ulong oldstate = _state;
+            _state = unchecked(oldstate *6364136223846793005U + _inc);
+            uint xorshifted = (uint)((oldstate >> 18) ^ oldstate) >> 27;
+            int rot = (int)(oldstate >> 59);
+            return (xorshifted >> rot) | (xorshifted << (-rot & 31));
+        }
+
+        public uint Range(uint bound)
+        {
+            uint threshold = (uint)((0x100000000UL - bound) % bound);
+
+            for (;;)
+            {
+                uint r = Random();
+                if (r >= threshold)
+                {
+                    return r % bound;
+                }
+            }
+        }
+
+        public override int Next()
+        {
+            return (int)Random();
+        }
+
+        public override int Next(int maxValue)
+        {
+            return (int)Range((uint)maxValue);
+        }
+
+        public override int Next(int minValue, int maxValue)
+        {
+            uint bound = (uint)(maxValue - minValue);
+            uint result = Range(bound);
+            return (int)(result + minValue);
+        }
+    }
+}
